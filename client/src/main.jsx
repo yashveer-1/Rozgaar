005d4514cb295ca771d8cf9b6bcc922811c4752e
Yashveer Singh <ys748477@gmail.com>
2026-07-04 17:30:44 +0000

    route error fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
 import App from './App';
@@ -11,10 +11,10 @@ const queryClient = new QueryClient({ defaultOptions: { queries: { staleTime: 30
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <Toaster position="top-right" richColors />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
